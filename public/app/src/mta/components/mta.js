029ebd29db767b7d142668cfc91624b994390412
Jon Zielenkievicz <jonzielen@gmail.com>
2017-07-22 14:11:03 -0400

    add data to controler

--- public/app/src/mta/components/mta.js
+++ public/app/src/mta/components/mta.js
@@ -2,29 +2,15 @@ class ComponentController {
   constructor($scope, dataFactory) {
     this.one = 'ONE';
     this.name = this.title;
-    //this.dataFactory = dataFactory;
-    //console.log('in the app 2');
-
-    // this.mtaData = {
-    //   one: 'one',
-    //   two: 'two',
-    //   three: 'three'
-    // };
-
-    //get album images
-    this.mtaData = dataFactory.getMtaData()
-      .then(response => {
-        console.log(response.stopData);
-        return response.stopData;
-      })
-      .catch(err => {
-        // error settings
-        console.log('error!');
-        return 'ERROR';
-    });
+    this.dataFactory = dataFactory;
   }
 
-
+  $onInit() {
+    this.dataFactory.getMtaData().then((response) => {
+    //console.log(response);
+    this.mtaData = response;
+    });
+  }
 }
 
 // dependency injection
